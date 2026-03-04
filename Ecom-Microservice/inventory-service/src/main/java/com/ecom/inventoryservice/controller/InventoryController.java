5e17afcc2aa2ec41313bb0271ba2003146d19d5a
Akash <akashk.bdvt1@gmail.com>
2026-03-05 00:25:55 +0530

    complete imp of resilience4j

--- Ecom-Microservice/inventory-service/src/main/java/com/ecom/inventoryservice/controller/InventoryController.java
+++ Ecom-Microservice/inventory-service/src/main/java/com/ecom/inventoryservice/controller/InventoryController.java
@@ -2,6 +2,8 @@ package com.ecom.inventoryservice.controller;
 
 import com.ecom.inventoryservice.dto.InventoryResponse;
 import com.ecom.inventoryservice.service.InventoryService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +13,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
+@Slf4j
 public class InventoryController {
 
     @Autowired
     private InventoryService inventoryService;
     @GetMapping
-    public ResponseEntity<List<InventoryResponse>> inventoryInfo(@RequestParam List<String> skuCode)
-    {
+    @SneakyThrows // don't use this; not recommended -> here used to see exception
+    public ResponseEntity<List<InventoryResponse>> inventoryInfo(@RequestParam List<String> skuCode) {
+        log.info("wait started");
+//        Thread.sleep(10000);
+        log.info("wait ended");
         return new ResponseEntity<>(inventoryService.isInStock(skuCode),HttpStatus.OK);
     }
 }
